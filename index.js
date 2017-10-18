1636f8bedf5b6810c5d6b9a7f680b40da7782fc9
Burak Yigit Kaya <ben@byk.im>
2017-10-18 21:39:43 +0100

    feat: Add commitizen support

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const nearley = require('nearley');
 const grammar = require('./lib/grammar');
 const {serialize} = require('./lib/serializer');
 const rank = require('./lib/rank');
+const engine = require('./lib/engine');
 
-// Create a Parser object from our grammar.
-const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+if (require.main === module) {
+  // Create a Parser object from our grammar.
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
-process.stdin.on('data', data => parser.feed(data.toString('utf8')));
-process.stdin.on('end', () => {
-  const rankedResults = rank(parser.results);
-  console.log(serialize(rankedResults[0]));
-});
+  process.stdin.on('data', data => parser.feed(data.toString('utf8')));
+  process.stdin.on('end', () => {
+    const rankedResults = rank(parser.results);
+    console.log(serialize(rankedResults[0]));
+  });
+} else {
+  module.exports = engine();
+}
